15fa25e824052e9f348a14dfca56df0d28b6b8a5
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-25 17:22:44 +0800

    Remove winlibs Toolchain.

--- prebuild.sh
+++ prebuild.sh
@@ -9,8 +9,8 @@ NAME=wxUiEditor-build${BUILD_DATE}
 cd /home
 
 # Toolchain used to build wxUiEditor
-wget -q https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
-7z x winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z -r -o/home
+#wget -q https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
+#7z x winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z -r -o/home
 
 git clone https://github.com/KeyWorksRW/wxUiEditor.git
 
