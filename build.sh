4a6f33b70e8742a1ba7b656f3e6117ab769f50d6
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-25 09:21:49 +0800

    Update build.sh

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ rm -rf /c/mingw64/bin
 cd /home
 
 # Toolchain used to build wxUiEditor
-wget https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
+wget -q https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
 7z x winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z -r -o/home
 cp -rf ./mingw32/* /mingw32/
 
