2667eb2024db07d5a19ad84ebcba8ea91229bd30
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-25 09:27:00 +0800

    A better way to use the downloaded compiler.

--- build.sh
+++ build.sh
@@ -6,15 +6,12 @@ BUILD_DATE=20240625
 HOME=$(cygpath -m /home)
 NAME=wxUiEditor-build${BUILD_DATE}
 
-# Delete the existing mingw64 at C: to avoid conflicting
-rm -rf /c/mingw64/bin
-
 cd /home
 
 # Toolchain used to build wxUiEditor
 wget -q https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
 7z x winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z -r -o/home
-cp -rf ./mingw32/* /mingw32/
+export PATH=/home/mingw32/bin/:$PATH
 
 git clone https://github.com/KeyWorksRW/wxUiEditor.git
 cd wxUiEditor/
