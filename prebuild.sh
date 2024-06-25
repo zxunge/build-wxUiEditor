aa9a3165ff6c5a0c511a4c8f04fb7715ff23940a
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-25 18:14:23 +0800

    Some funny tricks.

--- prebuild.sh
+++ prebuild.sh
@@ -12,6 +12,10 @@ cd /home
 #wget -q https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
 #7z x winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z -r -o/home
 
+# Funny tricks.
+rm -rf "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.40.33807/bin/Hostx64"
+mv -rf "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.40.33807/bin/Hostx86/x86" "C:/Program Files/Microsoft Visual Studio/2022/Enterprise/VC/Tools/MSVC/14.40.33807/bin/Hostx64/x64"
+
 git clone https://github.com/KeyWorksRW/wxUiEditor.git
 
 # Those build process should not be acted in msys2.
