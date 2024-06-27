e45157a025b2d01e8aba53ebb158bea3c4073310
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-27 12:26:56 +0800

    Regress to original build script.

---
+++ build.sh
@@ -0,0 +1,32 @@
+#!/usr/bin/bash
+
+set -eux
+
+BUILD_DATE=20240625
+HOME=$(cygpath -m /home)
+NAME=wxUiEditor-build${BUILD_DATE}
+
+cd /home
+
+# Toolchain used to build wxUiEditor
+#wget -q https://github.com/brechtsanders/winlibs_mingw/releases/download/14.1.0posix-18.1.7-12.0.0-ucrt-r2/winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z
+#7z x winlibs-i686-posix-dwarf-gcc-14.1.0-mingw-w64ucrt-12.0.0-r2.7z -r -o/home
+#export PATH=/home/mingw32/bin/:$PATH
+
+git clone https://github.com/KeyWorksRW/wxUiEditor.git
+cd wxUiEditor/
+
+# Build!
+cmake -G "Ninja Multi-Config" . -B build
+cmake --build build --config Release --target wxUiEditor
+
+# Compress results
+7zr a -mx9 -mqs=on -mmt=on /home/${NAME}.7z /home/wxUiEditor
+
+# Upload binaries
+if [[ -v GITHUB_WORKFLOW ]]; then
+  echo "OUTPUT_BINARY=${HOME}/${NAME}.7z" >> $GITHUB_OUTPUT
+  echo "RELEASE_NAME=${NAME}" >> $GITHUB_OUTPUT
+  echo "BUILD_DATE=${BUILD_DATE}" >> $GITHUB_OUTPUT
+  echo "OUTPUT_NAME=${NAME}.7z" >> $GITHUB_OUTPUT
+fi
