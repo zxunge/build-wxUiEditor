87bc0b241043709ca184fdd1bb5b061cfe22d84f
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-28 10:04:12 +0800

    Ninja.

--- cmake-ninja.sh
+++ cmake-ninja.sh
@@ -6,4 +6,6 @@ mkdir -p /d/temp
 cd /d/temp
 
 wget -q https://github.com/Kitware/CMake/releases/download/v3.28.6/cmake-3.28.6-windows-i386.zip
+wget -q https://github.com/ninja-build/ninja/releases/download/v1.12.1/ninja-win.zip
 unzip -q cmake-3.28.6-windows-i386.zip
+unzip -q ninja-win.zip -o ./ninja
