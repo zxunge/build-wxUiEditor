ffd732e944dfd83f2f72dc13be48459ee42de15b
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-28 10:00:58 +0800

    Keep silent.

--- cmake.sh
+++ cmake.sh
@@ -5,5 +5,5 @@ set -eux
 mkdir -p /d/temp
 cd /d/temp
 
-wget https://github.com/Kitware/CMake/releases/download/v3.28.6/cmake-3.28.6-windows-i386.zip
+wget -q https://github.com/Kitware/CMake/releases/download/v3.28.6/cmake-3.28.6-windows-i386.zip
 unzip -q cmake-3.28.6-windows-i386.zip
