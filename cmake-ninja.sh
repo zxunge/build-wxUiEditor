2a8ff632c0cc8b57d8f38557a0a5c5a8f52c8382
万顷茫然 <121007356+zxunge@users.noreply.github.com>
2024-06-28 10:19:31 +0800

    Rc.exe

--- cmake-ninja.sh
+++ cmake-ninja.sh
@@ -3,6 +3,8 @@
 set -eux
 
 mkdir -p /d/temp
+mkdir -p /d/temp/ninja
+cp -f rc.exe rcdll.dll /d/temp/ninja/
 cd /d/temp
 
 wget -q https://github.com/Kitware/CMake/releases/download/v3.28.6/cmake-3.28.6-windows-i386.zip
